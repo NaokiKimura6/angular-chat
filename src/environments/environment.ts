a162e44ce2f7e037a595a9f9d7610deeb875c2c5
Naoki Kimura <n-kimura@fores.jp>
2022-01-14 20:33:21 +0900

    途中コミット　一旦中断

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDvQMGKNZ61V9rgWiGrzxv0RU7lg0OR5nI',
+    authDomain: 'angular-chat-3b655.firebaseapp.com',
+    projectId: 'angular-chat-3b655',
+    storageBucket: 'angular-chat-3b655.appspot.com',
+    messagingSenderId: '636890918711',
+    appId: '1:636890918711:web:cbb3d0524c4938ea7134b2'
+  },
 };
 
 /*
